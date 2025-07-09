2b8c5df4a26484f8ad55ee93168f6eb10bbc4ee2
amdamv <akhmad.ak@betrium.io>
2025-07-09 11:14:25 +0200

    [fix]-014 change route of migrations

--- apps/user/src/databases/config/data-source.ts
+++ apps/user/src/databases/config/data-source.ts
@@ -12,6 +12,6 @@ export const dataSource = new DataSource({
   password: process.env.PG_PASSWORD || 'admin123',
   database: process.env.PG_DATABASE || 'PgWithNest',
   entities: [UserEntity],
-  migrations: ['src/migrations/*.ts'],
+  migrations: ['libs/my-lib/src/database/migrations/*.ts'],
   synchronize: false,
 });
